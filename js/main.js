96708aa1977cb5ea8fa4ad86ab11d5dcfc4cdfd5
ricmig <ricmig@hotmail.com>
2019-04-02 13:04:38 -0300

    foi adicionado o carousel na index.html

---
+++ js/main.js
@@ -0,0 +1,20 @@
+
+
+//   $('.owl-carousel').owlCarousel({
+//     items:1,
+//     margin:10,
+//     autoHeight:true
+// });
+
+$('.owl-carousel').owlCarousel({
+    animateOut: 'slideOutDown',
+    animateIn: 'fadeOutleft',
+    autoplay:true,
+    loop:true,
+    autoplayHoverPause:true,
+    autoplayTimeout:5000,
+    items:1,
+    margin:30,
+    autoHeight:true,
+    smartSpeed:450
+});
